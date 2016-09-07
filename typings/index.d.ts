8711ea8071a9292d035d7555c924763e647c729b
lostfields <ng@24sevenoffice.com>
2016-09-07 13:05:08 +0200

    adding typings for promise

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/es2015-promise/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="jsep/jsep.d.ts" />
