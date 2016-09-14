e2bbd8e2a8e0ef1a03f9fd93e690e38f2769baad
lostfields <ng@24sevenoffice.com>
2016-09-14 13:04:15 +0200

    Target ES6

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="globals/es2015-promise/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="jsep/jsep.d.ts" />
