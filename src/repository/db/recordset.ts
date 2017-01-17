21116c327090521f0421b5dc3d04aeea267fb579
lostfields <ng@24sevenoffice.com>
2017-01-17 14:03:13 +0100

    adding total length to recordset

--- src/repository/db/recordset.ts
+++ src/repository/db/recordset.ts
@@ -1,16 +1,24 @@
 ﻿export interface IRecordSet<TEntity> {
     affected: number
+    totalLength: number
     length: number
     executionTime: number
     records: TEntity[]
 }
 
 export class RecordSet<TEntity> implements IRecordSet<TEntity> {
-    constructor(public records: Array<TEntity>, public affected: number = 0, public executionTime: number = 0) {
+    constructor(records: Array<TEntity>, affected: number) 
+    constructor(records: Array<TEntity>, affected: number, executionTime: number) 
+    constructor(records: Array<TEntity>, affected: number, executionTime: number, totalLength: number) 
+    constructor(public records: Array<TEntity>, public affected: number = 0, public executionTime: number = 0, private _totalLength: number = -1) {
 
     }
 
     public get length() {
         return Array.isArray(this.records) ? this.records.length : 0;
+    }
+
+    public get totalLength() {
+        return this._totalLength == -1 ? this.length : this.totalLength;
     }
 }
