b1fd591eb94800589988b52a57326a5dd1721b05
24SevenOffice <post@24sevenoffice.com>
2018-02-22 06:09:41 +0000

    Adding release files

---
+++ lib/linq/expressions/unaryexpression.d.ts
@@ -0,0 +1,11 @@
+import { IUnaryExpression, UnaryOperatorType, UnaryAffixType } from './interfaces/iunaryexpression';
+import { IExpression, Expression } from './expression';
+export declare class UnaryExpression extends Expression implements IUnaryExpression {
+    operator: UnaryOperatorType;
+    affix: UnaryAffixType;
+    argument: IExpression;
+    constructor(operator: UnaryOperatorType, affix: UnaryAffixType, argument: IExpression);
+    equal(expression: IUnaryExpression): boolean;
+    toString(): string;
+}
+export { IUnaryExpression, UnaryOperatorType, UnaryAffixType };
