85a24a0404407aab42ff08b9b2410a2fbdf21e01
lostfields <ng@24sevenoffice.com>
2018-06-04 12:45:12 +0200

    renaming var to match other tests

--- src/test/javascriptvisitor.ts
+++ src/test/javascriptvisitor.ts
@@ -3,32 +3,32 @@ import * as Expr from './../linq/expressions/expressionvisitor';
 import { JavascriptVisitor } from './../linq/expressions/javascriptvisitor';
 
 describe("When using JavascriptVisitor", () => {
-    var parser: JavascriptVisitor,
+    var reducer: JavascriptVisitor,
         vars = { number: 5, string: 'abc', decimal: 5.50, date: new Date("2017-05-10T06:48:00Z"), object: { number: 7 } };
 
     beforeEach(() => {
-        parser = new JavascriptVisitor();
+        reducer = new JavascriptVisitor();
     })
 
     it("should evaluate a simple expression with binary operation", () => {
-        let reduced = parser.visitLambda(() => 2 + 3),
-            expr = parser.evaluate(reduced, vars);
+        let reduced = reducer.visitLambda(() => 2 + 3),
+            expr = reducer.evaluate(reduced, vars);
 
         assert.equal(expr.type, Expr.ExpressionType.Literal);
         assert.equal((<Expr.LiteralExpression>expr).value, 5);
     })
 
     it("should be able to do string operations at literal", () => {
-        let reduced = parser.visitLambda(() => "ABC".toLowerCase()),
-            expr = parser.evaluate(reduced, vars);
+        let reduced = reducer.visitLambda(() => "ABC".toLowerCase()),
+            expr = reducer.evaluate(reduced, vars);
 
         assert.equal(expr.type, Expr.ExpressionType.Literal);
         assert.equal((<Expr.LiteralExpression>expr).value, 'abc');
     })
 
     it("should be able to do string operations at variables", () => {
-        let reduced = parser.visitLambda(() => this.string.toUpperCase()),
-            expr = parser.evaluate(reduced, vars);
+        let reduced = reducer.visitLambda(() => this.string.toUpperCase()),
+            expr = reducer.evaluate(reduced, vars);
 
         assert.equal(expr.type, Expr.ExpressionType.Literal);
         assert.equal((<Expr.LiteralExpression>expr).value, 'ABC');
