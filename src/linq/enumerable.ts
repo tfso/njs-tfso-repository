262985c8d83ebe7bc45ab74d76ff81f42bceca2b
lostfields <nicolai.gjaerum@gmail.com>
2018-09-07 14:31:29 +0200

    documentation of IEnumerable

--- src/linq/enumerable.ts
+++ src/linq/enumerable.ts
@@ -28,6 +28,17 @@ export interface IEnumerable<TEntity> extends Iterable<TEntity>, AsyncIterable<T
     from(items: AsyncIterable<TEntity>)
 
     /**
+     * A remapper of identifier names, members is seperated with dot.
+     * @param remapper Function that returns the new name of the identifier
+     */
+    remap(remapper: (name: string) => string) : this
+    /**
+     * A remapper of values that corresponds to a identifier name
+     * @param remapper Function that returns the new value
+     */
+    remap(remapper: (name: string, value: any) => any) : this
+
+    /**
     * Where clause using OData $filter expression returning either true or false. Any parameters used is properties of TEntity
     * @param predicate OData expression
     */
@@ -39,13 +50,94 @@ export interface IEnumerable<TEntity> extends Iterable<TEntity>, AsyncIterable<T
      */
     where(predicate: (it: TEntity, ...param: any[]) => boolean, ...parameters: any[]): this
 
-    //orderBy(property: (it: TEntity) => void): this
+    orderBy(property: (it: TEntity) => void): this
+
     //range(start: number, count: number): this
+    
+    /**
+     * Bypasses a specified number of elements in a sequence and then returns the remaining elements
+     * @param count The number of elements to skip before returning the remaining elements
+     */
     skip(count: number): this
+
+    /**
+     * Bypassing elements using OData $filter expression as long as specified condition is true and then returns the remaining elements
+     * @param predicate OData expression
+     */
+    skipWhile(predicate: string): this
+    /**
+     * Bypassing elements using Javascript expression as long as specified condition is true and then returns the remaining elements
+     * @param predicate javascript expression
+     * @param parameters any javascript parameters has to be declared
+     */
+    skipWhile(predicate: (it: TEntity, ...param: any[]) => boolean, ...parameters: any[]): this
+
+    /**
+     * Returns a specified number of contiguous elements from the start of a sequence.
+     * @param count The number of elements to return
+     */
     take(count: number): this
     //reverse(): this
 
-    join<TInner, TResult>(inner: IEnumerable<TInner>, outerKey: (a: TEntity) => void, innerKey: (b: TInner) => void, selector: (outer: TEntity, inner: IEnumerable<TInner>) => TResult): IEnumerable<TResult>
+    /**
+     * Bypasses all elements before beginning and returns the remaining elements or up to the specified end
+     * @param begin zero-based index at which to begin extraction
+     * @param end zero-based index before which to end extraction
+     */
+    slice(begin: number, end?: number): this
+    /**
+     * Bypasses all elements before beginning and returns the remaining elements
+     * Note: If the beginning is not an index it's up to the repository to handle slicing.
+     * @param token a token that indicates where to begin extraction
+     */
+    slice(token: any): this    
+
+    /**
+     * returns a new IEnumerable of TResult (Left Join)
+     * @param inner
+     * @param outerKey
+     * @param innerKey
+     * @param selector A function that returns the new object, (outer, inner) => { outer, inner } or (outer, inner) => Object.assign({}, a, { childs: inner.toArray() })
+     * @param indexing If set to true an Array of outerKey will be passed into iterator of TInner as { keys: Array<any> }. Cons, all elements of outer iterator will be be kept in memory.
+     * @returns IEnumerable<TResult>
+     */
+    groupJoin<TInner, TResult>(inner: Iterable<TInner>, outerKey: (outer: TEntity) => void, innerKey: (inner: TInner) => void, selector: (outer: TEntity, inner: IEnumerable<TInner>) => TResult, indexing?: boolean): IEnumerable<TResult>
+    /**
+     * returns a new IEnumerable of TResult (Left Join)
+     * @param inner
+     * @param outerKey
+     * @param innerKey
+     * @param selector A function that returns the new object, (outer, inner) => { outer, inner } or (outer, inner) => Object.assign({}, a, { childs: inner.toArray() })
+     * @param indexing If set to true an Array of outerKey will be passed into iterator of TInner as { keys: Array<any> }. Cons, all elements of outer iterator will be be kept in memory.
+     * @returns IEnumerable<TResult>
+     */
+    groupJoin<TInner, TResult>(inner: AsyncIterable<TInner>, outerKey: (outer: TEntity) => void, innerKey: (inner: TInner) => void, selector: (outer: TEntity, inner: IEnumerable<TInner>) => TResult, indexing?: boolean): IEnumerable<TResult> 
+
+    /**
+     * returns a new IEnumerable of TResult (Inner Join)
+     * @param inner
+     * @param outerKey
+     * @param innerKey
+     * @param selector A function that returns the new object, (outer, inner) => { outer, inner } or (outer, inner) => Object.assign({}, a, { childs: inner.toArray() })
+     * @param indexing If set to true an Array of outerKey will be passed into iterator of TInner as { keys: Array<any> }. Cons, all elements of outer iterator will be be kept in memory.
+     * @returns IEnumerable<TResult>
+     */
+    join<TInner, TResult>(inner: Iterable<TInner>, outerKey: (outer: TEntity) => void, innerKey: (inner: TInner) => void, selector: (outer: TEntity, inner: IEnumerable<TInner>) => TResult, indexing?: boolean): IEnumerable<TResult> 
+    /**
+     * returns a new IEnumerable of TResult (Inner Join)
+     * @param inner
+     * @param outerKey
+     * @param innerKey
+     * @param selector A function that returns the new object, (outer, inner) => { outer, inner } or (outer, inner) => Object.assign({}, a, { childs: inner.toArray() })
+     * @param indexing If set to true an Array of outerKey will be passed into iterator of TInner as { keys: Array<any> }. Cons, all elements of outer iterator will be be kept in memory.
+     * @returns IEnumerable<TResult>
+     */
+    join<TInner, TResult>(inner: AsyncIterable<TInner>, outerKey: (outer: TEntity) => void, innerKey: (inner: TInner) => void, selector: (outer: TEntity, inner: IEnumerable<TInner>) => TResult, indexing?: boolean): IEnumerable<TResult>
+
+    /**
+    * returns a new IEnumerable of TResult
+    * @param selector
+    */
     select<TResult>(selector: (it: TEntity) => TResult): IEnumerable<TResult>
 
     first(items?: Array<TEntity>): TEntity
@@ -56,6 +148,8 @@ export interface IEnumerable<TEntity> extends Iterable<TEntity>, AsyncIterable<T
 
     toArrayAsync(items: Array<TEntity>): Promise<Array<TEntity>>
     toArrayAsync(): Promise<Array<TEntity>>
+
+    copy(): IEnumerable<TEntity>
 }
 
 export class Enumerable<TEntity> implements IEnumerable<TEntity>
