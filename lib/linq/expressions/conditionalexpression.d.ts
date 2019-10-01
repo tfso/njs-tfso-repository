fa44fac4edb2d3672dea1127be1c3b413325989f
24SevenOffice <post@24sevenoffice.com>
2019-10-01 11:11:21 +0000

    Adding release files

---
+++ lib/linq/expressions/conditionalexpression.d.ts
@@ -0,0 +1,14 @@
+import { IConditionalExpression } from './interfaces/iconditionalexpression';
+import { IExpression, Expression } from './expression';
+/**
+ * Not in use for now
+ */
+export declare class ConditionalExpression extends Expression implements IConditionalExpression {
+    condition: IExpression;
+    success: IExpression;
+    failure: IExpression;
+    constructor(condition: IExpression, success: IExpression, failure: IExpression);
+    equal(expression: IConditionalExpression): boolean;
+    toString(): string;
+}
+export { IConditionalExpression };
